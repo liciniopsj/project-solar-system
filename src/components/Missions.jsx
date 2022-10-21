de1ecdff9aa24b567119f5e6aa48ff3da9f3dce6
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-21 15:54:48 -0300

    Req 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Title from "./Title";
+import Title from './Title';
 // import MissionCard from "./MissionCard";
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        Placeholder
+        <Title headline="Missões" />
       </div>
     );
   }
